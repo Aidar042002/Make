e38506aff50793f190a3273c259c93bb55b4d737
Aidar042002 <71192036+Aidar042002@users.noreply.github.com>
2022-10-23 16:32:11 +0300

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Test");
+        int a =5;
+        int b= 5;
+        System.out.println(a+b);
     }
 }
